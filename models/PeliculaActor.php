88e82c6c6fbdb572226b0eb2993d3608ab16b923
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-18 22:06:10 -0500

    creation methods crud

--- models/PeliculaActor.php
+++ models/PeliculaActor.php
@@ -1,15 +1,34 @@
 <?php
+   require_once('../utils/Database.php');
 
-class PeliculaActor
+    class PeliculaActor
     {
         private $idPelicula;
         private $idActor;
 
-        function __construct($idPelicula, $idActor)
+        function constructor($idPelicula, $idActor)
         {
 
             $this->idPelicula = $idPelicula;
             $this->idActor = $idActor;
+            $this->database = new Database();
+        }
+
+        public function constructorVacio()
+        {
+            $this->database = new Database();
+        }
+
+        public function __construct()
+        {
+            $params = func_get_args();
+            $num_params = func_num_args();
+
+            if ($num_params == 0) {
+                call_user_func_array(array($this,'constructorVacio'),$params);
+            } else {
+                call_user_func_array(array($this,'constructor'),$params);
+            }
         }
 
         /**
@@ -43,6 +62,42 @@ class PeliculaActor
         {
             $this->idActor = $idActor;
         }
+
+        public function getAll()
+        {
+            $connection = $this->database->getConnection();
+
+            $query = "SELECT * FROM filaviu.pelicula_actores";
+            $result = $connection->query($query);
+            $listData = [];
+
+            foreach ($result as $item) 
+            {
+             $peliculaActor = new PeliculaActor($item['idPelicula'], $item['idActor']);
+             array_push($listData, $peliculaActor);
+            }
+
+            $this->database->closeConnection();
+            return $listData;
+        }
+
+        public function get()
+        {
+            $connection = $this->database->getConnection();
+
+            $query = "SELECT * FROM filaviu.pelicula_actores WHERE ID = ".$this->id;
+            $result = $connection->query($query);
+
+            $peliculaActor = null;
+            foreach ($result as $item) 
+            {
+             $peliculaActor = new PeliculaActor($item['idPelicula'], $item['idActor']);
+            }
+
+            $this->database->closeConnection();
+            return $peliculaActor;
+        }
+
 
     }
 ?>
