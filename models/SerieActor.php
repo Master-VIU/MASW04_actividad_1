ff028e131d32489ecb70bb2a9a2f88193d11e79a
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-19 20:15:55 -0500

    SerieActor completed

--- models/SerieActor.php
+++ models/SerieActor.php
@@ -1,18 +1,135 @@
 <?php
-
-
+    require_once('../utils/Database.php');
     class SerieActor
     {
         private $idSerie;
         private $idActor;
+        private $database;
 
-        function __construct($idSerieActor, $idActorSerie)
+        function constructor($idSerieActor, $idActorSerie)
         {
             $this->idSerie = $idSerieActor;
             $this->idActor = $idActorSerie;
+            $this->database = new Database();
         }
 
-              /**
+        public function constructorVacio()
+        {
+            $this->database = new Database();
+        }
+
+        public function __construct()
+        {
+            $params = func_get_args();
+            $num_params = func_num_args();
+
+            if ($num_params == 0) {
+                call_user_func_array(array($this,'constructorVacio'),$params);
+            } else {
+                call_user_func_array(array($this,'constructor'),$params);
+            }
+        }
+
+        private function getAll()
+        {
+            $connection = $this->database->getConnection();
+            $query = "SELECT * FROM filaviu.serie_actores";
+            $result = $connection->query($query);
+            $listData = [];
+
+            foreach ($result as $item)
+            {
+                $serieActor = new SerieActor($item['ID_SERIE'], $item['ID_ACTOR']);
+                array_push($listData, $serieActor);
+            }
+
+            $this->database->closeConnection();
+            return $listData;
+        }
+
+
+        public function get()
+        {
+            $connection = $this->database->getConnection();
+            $query = "SELECT * FROM filaviu.serie_actores WHERE ID_SERIE = ".$this->idSerieActor;
+            $result = $connection->query($query);
+
+            $serieActor = null;
+            foreach($result as $item)
+            {
+                $serieActor = new SerieActor($item['ID_SERIE'], $item['ID_ACTOR']);
+            }
+
+            $this->database->closeConnection();
+            return $serieActor;
+        }
+
+        private function create()
+        {
+            $serieActorCreada = false;
+            $connection = $this->database->getConnection();
+
+            if($resultInsert = $connection->query(
+                "INSERT INTO filaviu.serie_actores (ID_SERIE,ID_ACTOR) VALUES ('$this->idSerie')"
+            ))
+            {
+                $serieActorCreada = true;
+            }
+
+            $this->database->closeConnection();
+            return $serieActorCreada;
+        }
+
+        public function update()
+        {
+            $serieActorActualizado = false;
+            $connection = $this->database->getConnection();
+            $query = "UPDATE filaviu.serie_actores set ID_SERIE = '$this->idSerie', ID_ACTOR = '$this->idActor' WHERE ID_SERIE = ".$this->idSerie;
+
+            if($this->exists())
+            {
+                if($resultInsert = $connection->query($query))
+                {
+                    $serieActorActualizado = true;
+                }
+            }
+
+            $this->database->closeConnection();
+            return $serieActorActualizado;
+        }
+
+        
+        public function remove()
+        {
+            $serieActorBorrado = false;
+            $connection = $this->database->getConnection();
+            $query = "DELETE FROM filaviu.serie_actores WHERE ID_SERIE = ".$this->idSerie;
+
+            if($this->exists())
+            {
+                if($resultRemove = $connection->query($query))
+                {
+                    $serieActorBorrado = true;
+                }
+            }
+            $this->database->closeConnection();
+            return $serieActorBorrado;
+        }
+
+        public function exists()
+        {
+            $existeSerieActor = false;
+            $serieActor = $this->get();
+            if($serieActor != null)
+            {
+                $existeSerieActor = true;
+            }
+
+            return $existeSerieActor;
+        }
+        
+        
+        /**
          * @return mixed
          */
         public function getIdSerie()
