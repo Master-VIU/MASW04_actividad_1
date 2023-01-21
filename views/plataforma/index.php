bd6292fadacc6a337428008a2d1e0fb4c4361905
alopezarias <alopea08@estudiantes.unileon.es>
2023-01-21 18:46:41 +0100

    updated plataforma view

--- views/plataforma/index.php
+++ views/plataforma/index.php
@@ -1,4 +1,5 @@
 <div>
+    <link rel="stylesheet" href="../styles/main.css" type="text/css">
     <?php
         include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PlataformaController.php';
         $listaPlataformas = listarPlataformas();
@@ -6,37 +7,37 @@
         if (count($listaPlataformas) > 0)
         {
     ?>
-            <table class="tabla">
-                <thead>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Acciones</th>
-                </thead>
-                <tbody>
-                <?php
+            <div class="table_container">
+                <ul class="items_table">
+                    <li class="table-header">
+                        <div class="item_column">Id</div>
+                        <div class="item_column">Nombre</div>
+                        <div class="item_column">Acciones</div>
+                    </li>
+                    <?php
                     foreach($listaPlataformas as $plataforma)
-                    {
-                ?>
-                        <tr>
-                            <td><?php echo $plataforma->getId(); ?></td>
-                            <td><?php echo $plataforma->getNombre(); ?></td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a class="btn btn-success" href="edit.php?id=<?php echo $plataforma->getId(); ?>">
-                                        Editar
-                                    </a>
-                                    <form name="delete-plataforma" action="delete.php" method="POST" style="...">
-                                        <input type="hidden" name="platformId" value="<?php echo $plataforma->getId();?>"/>
-                                        <button type="submit" class="btn btn-danger">Borrar</button>
-                                    </form>
+                        {
+                        ?>
+                            <li class="table-row">
+                                <div class="item_column" data-label="Id"><?php echo $plataforma->getId(); ?></div>
+                                <div class="item_column" data-label="Nombre"><?php echo $plataforma->getNombre(); ?></div>
+                                <div class="item_column" data-label="Acciones">
+                                    <div class="btn-group" role="group">
+                                        <a class="btn btn-success" href="edit.php?id=<?php echo $plataforma->getId(); ?>">
+                                            Editar
+                                        </a>
+                                        <form name="delete-plataforma" action="delete.php" method="POST" style="...">
+                                            <input type="hidden" name="platformId" value="<?php echo $plataforma->getId();?>"/>
+                                            <button type="submit" class="btn btn-danger">Borrar</button>
+                                        </form>
+                                    </div>
                                 </div>
-                            </td>
-                        </tr>
-                <?php
+                            </li>
+                        <?php
                     }
-                ?>
-                </tbody>
-            </table>
+                    ?>
+                </ul>
+            </div>
     <?php
         }
         else
