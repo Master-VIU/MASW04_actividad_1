b55847682e70ac4cb215c60569757e37d773e9f5
alopezarias <alopea08@estudiantes.unileon.es>
2023-01-21 20:01:54 +0100

    updated plataforma view, updated plataforma new.php

--- views/plataforma/new.php
+++ views/plataforma/new.php
@@ -1,17 +1,54 @@
 <div class="row">
+    <link rel="stylesheet" href="../styles/main.css" type="text/css">
     <?php
+        include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PlataformaController.php';
 
+        $sendData =false;
+        $plataformaCreada = false;
+
+        if(isset($_POST['botonCrear']))
+        {
+            $sendData = true;
+        }
+        if($sendData)
+        {
+            if(isset($_POST['nombrePlataforma']))
+            {
+                $plataformaCreada = crearPlataforma($_POST['nombrePlataforma']);
+            }
+
+            if ($plataformaCreada)
+            {
+                echo "Plataforma creada correctamente.";
+            }
+            else
+            {
+                echo "Error en la creacion de la plataforma. Intentalo de nuevo.";
+            }
+        }
     ?>
-    <div class="col-12">
-        <h1>Nueva Plataforma</h1>
-    </div>
-    <div>
-        <form name="nueva_plataforma" action="" method="POST">
-            <div class="mb-3">
-                <label for="nombrePlataforma" class="form-label">Nombre</label>
-                <input id="nombrePlataforma" name="nombrePlataforma" type="text" placeholder="Introduce el nombre de la plataforma" class="form-control" required />
-            </div>
-            <input type="submit" value="Crear" class="btn btn-primary" name="botonCrear" />
-        </form>
-    </div>
+    <div class="table_container">
+        <ul class="items_table">
+            <li class="table-title">
+                <div class="item_column">
+                    <a class="btn btn-success" href="index.php">
+                        Volver
+                    </a>
+                </div>
+                <div class="item_column">NUEVA PLATAFORMA</div>
+                <div class="item_column"></div>
+            </li>
+            <form name="nueva_plataforma" action="" method="POST">
+                <li class="table-row">
+                    <div class="item_column">
+                        <label for="nombrePlataforma" class="form-label">Nombre</label>
+                    </div>
+                    <div class="item_column_wide">
+                        <input id="nombrePlataforma" name="nombrePlataforma" type="text" placeholder="Introduce el nombre de la plataforma" class="form-control" required />
+                    </div>
+                    <div class="item_column"></div>
+                </li>
+                <input style="float: right;" type="submit" value="Crear" class="btn btn-primary" name="botonCrear" />
+            </form>
+        </ul>
 </div>
