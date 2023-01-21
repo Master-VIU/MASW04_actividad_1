e29e479e9d158e5cbc6ea67250cd1be4329ffa05
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-21 13:49:59 -0500

    Creation of index and application of styles

--- views/idioma/index.php
+++ views/idioma/index.php
@@ -1,4 +1,5 @@
 <div>
+<link rel="stylesheet" href="../styles/main.css" type="text/css">
             <div>
                 <a class="btn btn-primary" href="new.php">Crear idioma</a>
             </div>
@@ -7,24 +8,26 @@
         $listaIdiomas = listarIdiomas();
         if (count($listaIdiomas) > 0)
         {
-    ?>
-            <table class="tabla">
-                <thead>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Acciones</th>
-                </thead>
-                <tbody>
+     ?>
+            <table class="table_container">
+            <ul class="items_table">
+                    <li class="table-header">
+                        <div class="item_column">Id</div>
+                        <div class="item_column">Idioma</div>
+                        <div class="item_column">Iso code</div>
+                        <div class="item_column">Acciones</div>
+                    </li>
                 <?php
                     foreach($listaIdiomas as $idioma)
                     {
                 ?>
-                        <tr>
-                            <td><?php echo $idioma->getId(); ?></td>
-                            <td><?php echo $idioma->getNombre(); ?></td>
-                            <td><?php echo $idioma->getIsoCode(); ?></td>
-                            <td>
-                                <div class="btn-group" role="group">
+                <li class="table-row">
+                        
+                        <div class="item_column" data-label="Id"><?php echo $idioma->getId(); ?></div>
+                        <div class="item_column" data-label="Nombre"><?php echo $idioma->getNombre(); ?></div>
+                        <div class="item_column" data-label="Iso"><?php echo $idioma->getIsoCode(); ?></div>
+                        <div class="item_column" data-label="Acciones">
+                             <div class="btn-group" role="group">
                                     <a class="btn btn-success" href="edit.php?id=<?php echo $idioma->getId(); ?>">
                                         Editar
                                     </a>
@@ -32,13 +35,13 @@
                                         <input type="hidden" name="idiformId" value="<?php echo $idioma->getId();?>"/>
                                         <button type="submit" class="btn btn-danger">Borrar</button>
                                     </form>
-                                </div>
-                            </td>
-                        </tr>
+                            </div>
+                    </div>
+                        
+                    </li>
                 <?php
                     }
                 ?>
-                </tbody>
             </table>
     <?php
         }
