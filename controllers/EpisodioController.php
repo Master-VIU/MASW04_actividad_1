2e22f4fe29a8da8da0a430e82a7b5f86e3240b67
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-22 22:08:31 -0500

    advance in EpisodioController

---
+++ controllers/EpisodioController.php
@@ -0,0 +1,38 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/models/Episodio.php';
+
+function listarEpisodios()
+{
+    $model = new Episodio();
+    $listarEpisodios = $model->getAll();
+    return $listarEpisodios;
+}
+
+function obtenerEpisodio($idEpisodio)
+{
+    $model = new Episodio($idEpisodio);
+    $episodioObjeto = $model->get();
+    return $episodioObjeto;
+}
+
+function crearEpisodio($episodio, $isoCode)
+{
+    $nuevoEpisodio = new Episodio(null, $episodio, $isoCode);
+    $episodioCreado = $nuevoEpisodio->create();
+    return $episodioCreado;
+}
+
+function actualizarEpisodio($idEpisodio, $nombreEpisodio, $isoCode)
+{
+    $episodio = new Episodio($idEpisodio, $nombreEpisodio, $isoCode);
+    $episodioActualizado = $episodio->update();
+    return $episodioActualizado;
+}
+
+function eliminarEpisodio($idEpisodio)
+{
+    $episodio = new Episodio($idEpisodio);
+    $episodioEliminado = $episodio->remove();
+    return $episodioEliminado;
+}
+?>
