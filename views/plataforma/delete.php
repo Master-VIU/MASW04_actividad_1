35db66fb42adcc1157ae514d99f6ef8815b2ac1b
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-22 22:20:46 -0500

    advances in views

--- views/plataforma/delete.php
+++ views/plataforma/delete.php
@@ -1,51 +1,24 @@
 <div>
     <?php
         include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PlataformaController.php';
-        $listaPlataformas = listarPlataformas();
-
-        if (count($listaPlataformas) > 0)
-        {
-    ?>
-            <table class="tabla">
-                <thead>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Acciones</th>
-                </thead>
-                <tbody>
-                <?php
-                    foreach($listaPlataformas as $plataforma)
-                    {
-                ?>
-                        <tr>
-                            <td><?php echo $plataforma->getId(); ?></td>
-                            <td><?php echo $plataforma->getNombre(); ?></td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a class="btn btn-success" href="edit.php?id=<?php echo $plataforma->getId(); ?>">
-                                        Editar
-                                    </a>
-                                    <form name="delete-plataforma" action="delete.php" method="POST" style="...">
-                                        <input type="hidden" name="platformId" value="<?php echo $plataforma->getId();?>"/>
-                                        <button type="submit" class="btn btn-danger">Borrar</button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                <?php
-                    }
-                ?>
-                </tbody>
-            </table>
-    <?php
-        }
-        else
-        {
-    ?>
-            <div class="alerta alerta-warning" role="alert">
-                Aun no existen plataformas.
+        $plataforma = $_POST['platformId'];
+        $plataformaEliminada = eliminarPlataforma($plataforma);
+        if($plataformaEliminada)
+        {    
+            ?>
+            <div class="alert alert-success" role="alert">
+                Plataforma eliminada con éxito! <br><br>
+                <a href="index.php"> Volver al listado de plataformas.</a>
             </div>
-    <?php
+        <?php
+        }
+        else{
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    El plataforma no se ha borrado correctamente. <br><br>
+                    <a href="index.php"> Volver a intentarlo</a>
+                </div>
+            <?php
         }
     ?>
 </div>
