cdb8650f9bc7c1e9c8b3ef2116a3231bb2af82ec
CalixtoRuas <calixtoruas@hotmail.com>
2023-01-23 00:43:10 -0500

    modelo controlador y vista portada.

---
+++ controllers/portadacontroller.php
@@ -0,0 +1,43 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/models/Portada.php';
+
+function listarportada()
+{
+   $model =new Portada(null,null,null);
+   $listarportada = $model -> getAll();
+return $listarportada;
+}
+
+
+function  crearportada($tamanio, $imagen)
+{
+   $nuevaportada = new Portada(null, $tamanio, $imagen);
+   $portadaCreada = $nuevaportada->create();
+   return $portadaCreada;
+}
+
+
+function obtenerportada($idportada)
+{
+    $model = new Portada($idportada,null,null);
+    $PortadaObjeto = $model->get();
+    return $PortadaObjeto;
+}
+
+function actualizarPortada($id, $tamanio, $imagen)
+{
+    $portada = new Portada($id, $tamanio, $imagen);
+    $PortadaActualizada = $portada->update();
+    return $PortadaActualizada;
+}
+
+function eliminarPortada($id)
+{
+    $portada = new Portada($id,null,null);
+    $PortadaEliminada = $portada->remove();
+    return $PortadaEliminada;
+}
+?>
+
+
+?>
