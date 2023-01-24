b69381014daabf16cff50b4619cea696b3202a93
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-23 21:42:03 -0500

    Update edit.php

--- views/idioma/edit.php
+++ views/idioma/edit.php
@@ -1,31 +1,5 @@
 <div>
 <link rel="stylesheet" href="../styles/main.css" type="text/css">
-    <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/IdiomaController.php';
-    ?>
-    <body>
-        <div>
-            <?php
-                $idIdioma = $_GET['id'];
-                $idiomaObjeto = obtenerIdioma($idIdioma);
-
-                $sendData = false;
-                $idiomaEditado =false;
-                if(isset($_POST['botonEditar']))
-                {
-                    $sendData = true;
-                }
-                if($sendData)
-                {
-                    if(isset($_POST['editarIdioma']) && isset($_POST['editarIso']))
-                    {                        
-                        $idiomaEditado = actualizarIdioma($_POST['idiomaId'], $_POST['editarIdioma'], $_POST['editarIso']);
-                    }
-                }
-                if(!$sendData)
-                {                    
-            ?>
-
 <div class="table_container">
         <ul class="items_table">
             <li class="table-title">
@@ -37,7 +11,48 @@
                 <div class="item_column">EDITAR IDIOMA</div>
                 <div class="item_column"></div>
             </li>
-            <form name="editar_idioma" action="" method="POST">
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/IdiomaController.php';
+   
+        $idIdioma = $_GET['id'];
+        $idiomaObjeto = obtenerIdioma($idIdioma);
+
+        $sendData = false;
+        $idiomaEditado =false;
+        if(isset($_POST['botonEditar']))
+         {
+            $sendData = true;
+        }
+        if($sendData)
+        {
+            if(isset($_POST['editarIdioma']) && isset($_POST['editarIso']))
+            {                        
+                $idiomaEditado = actualizarIdioma($_POST['idiomaId'], $_POST['editarIdioma'], $_POST['editarIso']);
+            }
+        if($idiomaEditado)
+        {
+        ?>                
+        <li class="table-success">
+             <div class="item_column">Idioma editado correctamente.</div>
+        </li>
+        <?php
+        }
+        else
+        {
+        ?>
+        <li class="table-wrong">
+            <div class="item_column">
+                El idioma no se ha editado correctamente.
+                <a href="edit.php?id=<?php echo $idIdioma;?>"> Volver a intentarlo.</a>
+            </div>
+        </li>
+        <?php
+            }
+        }
+        else
+        {
+        ?>
+        <form name="editar_idioma" action="" method="POST">
             <li class="table-row">
                     <div class="item_column">
                         <label for="editarIdioma" class="form-label">Nombre</label>
@@ -57,36 +72,9 @@
                     </li>
                 <input style="float: right;" type="submit" value="Editar" class="btn btn-primary" name="botonEditar" />
             </form>
-        </ul>
-     </div>
-     <?php
-        }
-        else
-        {
-            if($idiomaEditado)
-                 {
-                    ?>
-                    <div class="row">
-                        <div class="alerta alerta-success" role="alert">
-                            Idioma editado correctamente. <br><br>
-                            <a href="index.php">Volver al listado de idiomas</a>
-                        </div>
-                    </div>
-                <?php
-                    }
-                    else
-                    {
-                 ?>
-                 <div class="row">
-                    <div class="alerta alerta-danger" role="alert">
-                        El idioma no se ha editado correctamente. <br><br>
-                        <a href="edit.php?id=<?php echo $idIdioma;?>"> Volver a intentarlo.</a>
-                    </div>
-                 </div>
-                 <?php
-                    }
-                }
-            ?>
-        </div>
-    </body>
-</div>
+            <?php
+           }
+         ?>
+    </ul>
+   </div>
+</div> 
