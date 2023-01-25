a51c7f4fb2336b867024febae3c152a6a3240f73
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-24 19:32:35 -0500

    Update Temporada.php

--- models/Temporada.php
+++ models/Temporada.php
@@ -1,18 +1,58 @@
 <?php
-
+require_once($_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/utils/Database.php');
 class Temporada
     {
         private $numero;
         private $serieId;
         private $id;
         private $fechaLanzamiento;
+        private $database;
 
-        function __construct($numeroTemporada, $serieIdTemporada, $idTemporada, $fechaLanzamientoTemporada)
+        function constructor($numeroTemporada, $serieIdTemporada, $idTemporada, $fechaLanzamientoTemporada)
         {
             $this->numero = $numeroTemporada;
             $this->serieId = $serieIdTemporada;
             $this->id = $idTemporada;
             $this->fechaLanzamiento = $fechaLanzamientoTemporada;
+            $this->database = Database::getInstance();
+        }
+
+        public function constructorVacio()
+        {
+            $this->database = Database::getInstance();
+        }
+
+        public function __construct()
+        {
+            $params = func_get_args();
+            $num_params = func_num_args();
+
+            if ($num_params == 0)
+            {
+                call_user_func_array(array($this,'constructorVacio'),$params);
+            }
+            else
+            {
+                call_user_func_array(array($this,'constructor'),$params);
+            }
+        }
+
+        public function getAll()
+        {
+            $connection = $this->database->getConnection();
+
+            $query = "SELECT * FROM filmaviu.temporadas";
+            $result = $connection->query($query);
+            $listData = [];
+
+            foreach ($result as $item)
+            {
+                $temporada = new Temporada($item['NUMERO'], $item['SERIE_ID'], $item['ID'],$item['FECHA_LANZAMIENTO']);
+                array_push($listData, $temporada);
+            }
+
+            $this->database->closeConnection();
+            return $listData;
         }
 
         /**
