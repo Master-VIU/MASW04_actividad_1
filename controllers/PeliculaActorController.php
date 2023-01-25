c643b99b8f21831c52e3be22ac289e3fa23e38d1
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-25 17:30:59 -0500

    Create PeliculaActorController.php

---
+++ controllers/PeliculaActorController.php
@@ -0,0 +1,38 @@
+<?php
+    require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/models/PeliculaActor.php');
+
+    function listarPeliculaActores()
+    {
+        $model = new PeliculaActor();
+        $listaPeliculaActores = $model->getAll();
+        return $listaPeliculaActores;
+    }
+
+    function obtenerPeliculaActor($idPeliculaActor)
+    {
+        $model = new PeliculaActor($idPeliculaActor, null);
+        $peliculaActorObjeto = $model->get();
+        return $peliculaActorObjeto;
+    }
+    function crearPeliculaActor($pelicula, $actor)
+    {
+        $nuevaPeliculaActor = new PeliculaActor($pelicula, $actor);
+        $peliculaActorCreada = $nuevaPeliculaActor->create();
+        return $peliculaActorCreada;
+    }
+
+    function actualizarPeliculaActor($idPeliculaActor, $nombrePeliculaActor)
+    {
+        $peliculaActor = new PeliculaActor($idPeliculaActor, $nombrePeliculaActor);
+        $peliculaActorActualizada = $peliculaActor->update();
+        return $peliculaActorActualizada;
+    }
+
+    function eliminarPeliculaActor($idPeliculaActor)
+    {
+        $peliculaActor = new PeliculaActor($idPeliculaActor, null);
+        $peliculaActorEliminada = $peliculaActor->remove();
+        return $peliculaActorEliminada;
+    }
+
+?>
