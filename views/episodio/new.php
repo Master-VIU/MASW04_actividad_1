bc35c898142259a6e27aae32e8a9ad1013e85d93
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-27 16:02:07 -0500

    Update new.php

--- views/episodio/new.php
+++ views/episodio/new.php
@@ -26,7 +26,7 @@
     {
         if(isset($_POST['nuevaTemporada']) && isset($_POST['nuevoNumero'])&&isset($_POST['nuevoTitulo'])&&isset($_POST['nuevaDuracion']))
         {
-            $episodioCreado = crearEpisodio($_POST['nuevaTemporada'], $_POST['nuevoNumero'], $_POST['nuevoTitulo'], $_POST['nuevaduracion']);
+            $episodioCreado = crearEpisodio($_POST['nuevaTemporada'], $_POST['nuevoNumero'], $_POST['nuevoTitulo'], $_POST['nuevaDuracion']);
         }
 
         if($episodioCreado)
@@ -34,7 +34,7 @@
             
             ?>
             <li class="table-success">
-                <div class="item_column">Idioma creado correctamente.</div>
+                <div class="item_column">Episodio creado correctamente.</div>
                 </li>
             <?php
         }
@@ -42,40 +42,51 @@
         {
             ?>
             <li class="table-wrong">
-                <div class="item_column">No se ha podido crear el idioma. Inténtalo de nuevo.</div>
+                <div class="item_column">No se ha podido crear el episodio. Inténtalo de nuevo.</div>
             </li>
             <?php
         }
     }
 ?>            
-       <form name="nuevo_idioma" action="" method="POST">
-            <li class="table-row-form">
-                <div class="item_column">
+       <form name="nuevo_episodio" action="" method="POST">
+       <li class="table-row-form">
+                    <div class="item_column">
                     <label for="nuevaTemporada" class="form-label">Temporada</label>
                 </div>
                 <div class="item_column_wide">
-                    <input id="nuevaTemporada" name="nuevaTemporada" type="text" placeholder="Introduce la Temporada" class="form-control" required />
+                    <input id="nuevaTemporada" name="nuevaTemporada" type="text" placeholder="Introduce la Temporada" class="form-control" 
+                    required />
                 </div>
-                <div class="item_column_wide">
-                    <label for="nuevoNumero" class="form-label">Numero</label>
+                <div class="item_column"></div>
+        </li>
+        <li class="table-row-form">
+            <div class="item_column">
+                    <label for="nuevoNumero" class="form-label">Número</label>
+            </div>
+            <div class="item_column_wide">            
+                    <input id="nuevoNumero" name="nuevoNumero" type="text" placeholder="Introduce el # episodio" class="form-control"
+                     required />
                 </div>
-                <div>
-                    <input id="nuevoNumero" name="nuevoNumero" type="text" placeholder="Introduce el # episodio" class="form-control" required />
-                </div>
-                <div class="item_column_wide">
+                <div class="item_column"></div>
+            </li>
+        <li class="table-row-form">
+        <div class="item_column">
                     <label for="nuevoTitulo" class="form-label">Titulo</label>
-                </div>
-                <div>
+            </div>
+            <div class="item_column_wide">
                     <input id="nuevoTitulo" name="nuevoTitulo" type="text" placeholder="Introduce el Titulo" class="form-control" required />
                 </div>
-                <div class="item_column_wide">
-                    <label for="nuevaDuracion" class="form-label">Duracion</label>
-                </div>
-                <div>
-                    <input id="nuevaDuracion" name="nuevaDuracion" type="text" placeholder="Introduce La duracion" class="form-control" required />
-                </div>
-                
+                <div class="item_column"></div>
             </li>
+        <li class="table-row-form">
+        <div class="item_column">
+                    <label for="nuevaDuracion" class="form-label">Duracion</label>
+        </div>
+        <div class="item_column_wide">
+                    <input id="nuevaDuracion" name="nuevaDuracion" type="text" placeholder="Introduce La duracion" class="form-control" required />
+                    </div>
+                    <div class="item_column"></div>
+                </li>
             <input style="float: right;" type="submit" value="Crear" class="btn btn-primary" name="botonCrear" />
             </form>        
         </ul>
