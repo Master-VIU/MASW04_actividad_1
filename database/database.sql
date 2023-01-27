6d48b800f9f27d4937428e62fb8be9103abf4477
alopezarias <alopea08@estudiantes.unileon.es>
2023-01-27 20:52:43 +0100

    last changes to deliver

--- database/database.sql
+++ database/database.sql
@@ -262,11 +262,7 @@ VALUES
     (4, '/images/4.png'),
     (5, '/images/5.png'),
     (6, '/images/6.png'),
-    (7, '/images/7.png'),
-    (8, '/images/8.png'),
-    (9, '/images/9.png'),
-    (10, '/images/10.png'),
-    (11, '/images/11.png');
+    (7, '/images/7.png');
 
 INSERT INTO filmaviu.series (ID, TITULO, PLATAFORMA, DIRECTOR, CLASIFICACION, GENERO, PORTADA)
 VALUES
