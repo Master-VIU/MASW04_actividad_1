81bf96d6b196f533a2cc5a5e601e85d7e5089604
Mayerly Bustos <bustosrobayom@gmail.com>
2023-01-27 12:12:14 -0500

    Update index.php

--- views/serie/index.php
+++ views/serie/index.php
@@ -1,12 +1,11 @@
 <div>
     <link rel="stylesheet" href="../styles/main.css" type="text/css">
     <?php
-        require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PeliculaController.php');
+        require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/SerieController.php');
         require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PlataformaController.php');
-        require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PeliculaController.php');
         require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/ClasificacionController.php');
         require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/DirectorController.php');
-        //require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/GeneroController.php');
+        require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/GeneroController.php');
         require_once( $_SERVER['DOCUMENT_ROOT'].'/MASW04_actividad_1/controllers/PortadaController.php');
     ?>
             <div class="table_container">
@@ -17,7 +16,7 @@
                                 Volver
                             </a>
                         </div>
-                        <div class="item_column">PELICULAS</div>
+                        <div class="item_column">SERIES</div>
                         <div class="item_column">
                             <a class="btn btn-success" href="new.php">
                                 Crear
@@ -29,53 +28,47 @@
                         <div class="item_column">Titulo</div>
                         <div class="item_column">Plataforma Id</div>
                         <div class="item_column">Director Id</div>
-                        <div class="item_column">Puntuacion</div>
                         <div class="item_column">Clasificacion Id</div>
                         <div class="item_column">Genero Id</div>
                         <div class="item_column">Portada Id</div>
-                        <div class="item_column">Duracion</div>
                         <div class="item_column">Acciones</div>
                     </li>
                     <?php
-                    $listaPeliculas = listarPeliculas();
+                    $listaSeries = listarSeries();
 
-                    if (count($listaPeliculas) > 0)
+                    if (count($listaSeries) > 0)
                     {
-                    foreach($listaPeliculas as $pelicula)
+                    foreach($listaSeries as $serie)
                         {
                         ?>
                             <li class="table-row">
-                                <div class="item_column" data-label="Id"><?php echo $pelicula->getId(); ?></div>
-                                <div class="item_column" data-label="Titulo"><?php echo $pelicula->getTitulo(); ?></div>
+                                <div class="item_column" data-label="Id"><?php echo $serie->getId(); ?></div>
+                                <div class="item_column" data-label="Titulo"><?php echo $serie->getTitulo(); ?></div>
                                 <div class="item_column" data-label="PlataformaId">
-                                    <?php echo obtenerPlataforma($pelicula->getPlataformaId())->getNombre(); ?>
+                                    <?php echo obtenerPlataforma($serie->getPlataformaId())->getNombre(); ?>
                                 </div>
                                 <div class="item_column" data-label="DirectorId">
-                                    <?php $director = obtenerDirector($pelicula->getDirectorId());
+                                    <?php $director = obtenerDirector($serie->getDirectorId());
                                     echo $director->getNombre()." ".$director->getApellidos(); ?>
                                 </div>
-                                <div class="item_column" data-label="Puntuacion"><?php echo $pelicula->getPuntuacion(); ?></div>
                                 <div class="item_column" data-label="ClasificacionId">
-                                    <?php echo obtenerClasificacion($pelicula->getClasificacionId())->getTipo(); ?>
+                                    <?php echo obtenerClasificacion($serie->getClasificacionId())->getTipo(); ?>
                                 </div>
                                 <div class="item_column" data-label="GeneroId">
-                                    <!--?php echo obtenerGenero($pelicula->getGeneroId())->getNombre(); ?-->
+                                    <?php echo obtenerGenero($serie->getGeneroId())->getNombre(); ?>
                                 </div>
                                 <div class="item_column" data-label="PortadaId">
-                                    <?php $portada = obtenerPortada($pelicula->getPortadaId()); ?>
+                                    <?php $portada = obtenerPortada($serie->getPortadaId()); ?>
                                     <img src="http://localhost:8888/MASW04_actividad_1<?php echo $portada->getImagen(); ?>"
                                     width="150" height="150"/>
                                 </div>
-                                <div class="item_column" data-label="Duracion">
-                                    <?php echo gmdate("H:i:s", $pelicula->getDuracion()); ?>
-                                </div>
                                 <div class="item_column" data-label="Acciones">
                                     <div class="btn-group" role="group">
-                                        <a class="btn btn-success" href="edit.php?id=<?php echo $pelicula->getId(); ?>">
+                                        <a class="btn btn-success" href="edit.php?id=<?php echo $serie->getId(); ?>">
                                             Editar
                                         </a>
-                                        <form name="delete-pelicula" action="delete.php" method="POST" style="...">
-                                            <input type="hidden" name="peliculaId" value="<?php echo $pelicula->getId();?>"/>
+                                        <form name="delete-serie" action="delete.php" method="POST" style="...">
+                                            <input type="hidden" name="serieId" value="<?php echo $serie->getId();?>"/>
                                             <button type="submit" class="btn btn-danger">Borrar</button>
                                         </form>
                                     </div>
@@ -88,7 +81,7 @@
                 {
                     ?>
                     <li class="table-wrong">
-                        <div class="item_column">Aun no existen peliculas.</div>
+                        <div class="item_column">Aun no existen series.</div>
                     </li>
                     <?php
                 }
